be247550bc0b118a256026b41fb481ae1d68d885
Treas Anhira <zandy.yudha.p@gmail.com>
2018-01-31 08:18:07 +0700

    Update logic for writing to sd card one time once in interval logging time.

--- src/main.cpp
+++ src/main.cpp
@@ -15,22 +15,32 @@ void setup() {
 }
 
 void loop() {
+  struct Data *temp;
+  static bool hasWritten = false;
+
   System::loop();
   Indicating::loop();
   Sensor::loop();
-  Indicating::LCD::measurement(Indicating::RTC::getHour(), Indicating::RTC::getMinute(), Indicating::RTC::getHalfSecond(), 0, Sensor::DHT_11::getTemperature(), Sensor::DHT_11::getHumidity());
+  Indicating::LCD::measurement(Indicating::RTC::getHour(), Indicating::RTC::getMinute(), Indicating::RTC::getHalfSecond(), 0, temp->temperature, temp->humidity);
+
+  temp = &Sensor::DHT_11::getDHTData();
 
   static unsigned long lastSleep = millis();
   static bool isTimeout = false;
   unsigned int currentMinuteInDay = ((unsigned int) Indicating::RTC::getHour() * 60) + (unsigned int) Indicating::RTC::getMinute();
   isTimeout = (millis() - lastSleep);
 
-  if ((currentMinuteInDay % LOGGING_INTERVAL) == 0) {
-    char buffer[30];
-    Storage::SdCard::implode(buffer, &Sensor::DHT_11::getDHTData(), (const char*) ',');
-    Storage::SdCard::writeFile((char*) "DHT_LOGGER", Storage::SdCard::implode(buffer, &Sensor::DHT_11::getDHTData(), (const char*) ','), strlen(buffer));
-    // Storage::SdCard::implode(buffer, (char *)Indicating::RTC::getDateTimeSQLFormat(), Sensor::DHT_11::getTemperature(), Sensor::DHT_11::getHumidity(), (const char*) ',');
+  if (((currentMinuteInDay % LOGGING_INTERVAL) == 0)) {
+    if (Storage::SdCard::isinitialized() && !hasWritten) {
+      char buffer[30];
+      Storage::SdCard::implode(buffer, temp, ",");
+      Storage::SdCard::writeFile((char*) "DHT_LOGGER", Storage::SdCard::implode(buffer, &Sensor::DHT_11::getDHTData(), (const char*) ','), strlen(buffer));
+      hasWritten = true;
+    }
+    return;
   }
+  hasWritten = false;
+  
   if (isTimeout) {
     /* code */
   }
